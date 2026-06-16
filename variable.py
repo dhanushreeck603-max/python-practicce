24b855d36e749626e2d3bb600572019cfd94f128
DHANUSHREE C K <dhanushreeck603@gmail.com>
2026-06-16 08:56:02 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,17 @@
+name="dhanu"
+age =20
+area="banglore"
+contact=7700000000
+qulification="BCA"
+university name="mysore"
+height=5.2
+hobbies="lisingmusic"
+intrest="sports"
+print(name)
+print(age)
+print(area)
+print(contact)
+print(qualification)
+print(university name)
+print(height)
+print(intrest)
